f745da560851f9c20d07ca1f8ce29defef3196c3
FDC-9\HP <success_anil@yahoo.co.in>
2020-09-25 13:17:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/samplecouchapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.samplecouchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
