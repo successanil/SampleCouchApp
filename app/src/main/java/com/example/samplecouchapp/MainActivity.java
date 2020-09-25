233eb988978812aee7f346132224b0dc79cf5960
FDC-9\HP <success_anil@yahoo.co.in>
2020-09-25 13:23:57 +0530

    added bindings

--- app/src/main/java/com/example/samplecouchapp/MainActivity.java
+++ app/src/main/java/com/example/samplecouchapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.samplecouchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
         addServerData = findViewById(R.id.updateServerData);
         fetchServerData = findViewById(R.id.fetchServerData);
         resutls = findViewById(R.id.results);
+
+        addServerData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // sendata to server from editText
+                String str = addNameHereET.getText().toString();
+            }
+        });
+
+        fetchServerData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // fetch data from server and show here
+
+            }
+        });
     }
 }
