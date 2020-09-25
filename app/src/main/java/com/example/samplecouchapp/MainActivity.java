1ed309a80075218678cfc06d48925adf82bc3661
FDC-9\HP <success_anil@yahoo.co.in>
2020-09-25 13:22:26 +0530

    added bindings

--- app/src/main/java/com/example/samplecouchapp/MainActivity.java
+++ app/src/main/java/com/example/samplecouchapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.samplecouchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText addNameHereET;
+    Button addServerData;
+    Button fetchServerData;
+    TextView resutls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addNameHereET = findViewById(R.id.addNameHereET);
+        addServerData = findViewById(R.id.updateServerData);
+        fetchServerData = findViewById(R.id.fetchServerData);
+        resutls = findViewById(R.id.results);
     }
 }
